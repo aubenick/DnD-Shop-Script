e40a8d1446d3c2874455e0f72863a2485b31b837
aubenick <aubenick@gmail.com>
2023-07-05 10:03:00 -0400

    New Item

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
     # quantityGenerated = [2, 7, 3, 2, 0]
     # createShop(quantityGenerated)
 
-    addNewItem("Ioun Blade", veryrare)
+    addNewItem("Ouroboros Wand", rare)
 
 
 if __name__ == "__main__":
