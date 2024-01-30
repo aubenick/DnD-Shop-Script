cef1f5743b4e28c5a2bdcbc238d75b9faf5643b8
aubenick <aubenick@gmail.com>
2024-01-30 11:53:24 -0500

    New Item

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
 
     #createShop([0, 0, 7, 3, 1])
 
-    addNewItem("Cryomancer's Lexicon", veryrare)
+    addNewItem("Staff of the Eclipse", veryrare)
 
 
 if __name__ == "__main__":
