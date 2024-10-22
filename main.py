e81e3cc5dac586358d68ee5603bf5dbfdf741a01
aubenick <aubenick@gmail.com>
2024-10-22 14:11:01 -0400

    New Item

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
 
     #createShop([0, 0, 7, 3, 1])
 
-    addNewItem("Trickster's Quiver", rare)
+    addNewItem("Armor of the Platinum Lord", legendary)
 
 
 if __name__ == "__main__":
